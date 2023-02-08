60e85aac6630bd592f52866e76c7355bd6a8decf
KadaGuetouache <wail@disroot.org>
2023-02-08 19:09:19 +0100

    convert data into serializable data

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,54 +1,53 @@
 import Product from '../models/product';
 
 const PRODUCTS = [
-    new Product(
-        'p1',
-        'u1',
-        'Red Shirt',
-        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-        'A red t-shirt, perfect for days with non-red weather.',
-        29.99
-    ),
-    new Product(
-        'p2',
-        'u1',
-        'Blue Carpet',
-        'https://images.pexels.com/photos/6292/blue-pattern-texture-macro.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'Fits your red shirt perfectly. To stand on. Not to wear it.',
-        99.99
-    ),
-    new Product(
-        'p3',
-        'u2',
-        'Coffee Mug',
-        'https://images.pexels.com/photos/160834/coffee-cup-and-saucer-black-coffee-loose-coffee-beans-160834.jpeg?cs=srgb&dl=bean-beans-black-coffee-160834.jpg&fm=jpg',
-        'Can also be used for tea!',
-        8.99
-    ),
-    new Product(
-        'p4',
-        'u3',
-        'The Book - Limited Edition',
-        'https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?cs=srgb&dl=blur-blurred-book-pages-46274.jpg&fm=jpg',
-        "What the content is? Why would that matter? It's a limited edition!",
-        15.99
-    ),
-    new Product(
-        'p5',
-        'u3',
-        'PowerBook',
-        'https://get.pxhere.com/photo/laptop-computer-macbook-mac-screen-water-board-keyboard-technology-air-mouse-photo-airport-aircraft-tablet-aviation-office-black-monitor-keys-graphic-hardware-image-pc-exhibition-multimedia-calculator-vector-water-cooling-floppy-disk-phased-out-desktop-computer-netbook-personal-computer-computer-monitor-electronic-device-computer-hardware-display-device-448748.jpg',
-        'Awesome hardware, crappy keyboard and a hefty price. Buy now before a new one is released!',
-        2299.99
-    ),
-    new Product(
-        'p6',
-        'u1',
-        'Pen & Paper',
-        'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
-        "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
-        5.49
-    )
-];
+    {
+        "id": 'p1',
+        "ownerId": 'u1',
+        "title": 'Men\'s Full Sleeve Travel shirt',
+        "imageUrl": 'https://contents.mediadecathlon.com/p1484240/ab565f3675dbdd7e3c486175e2c16583/p1484240.jpg?format=auto&quality=70&f=650x0',
+        "description": 'After washing 3-4 times shirt colours faded. Even I dry it not directly on sunlingt. Comfortable but not recommended of this type faded shirt after 3-4 times wash.',
+        "price": 29.99
+    },
+    {
+        "id": 'p2',
+        "ownerId": 'u1',
+        "title": 'Adult Ski Beanie Black',
+        "imageUrl": 'https://contents.mediadecathlon.com/p1534668/45ba2405ad86d901c865a5cd38b2aa30/p1534668.jpg?format=auto&quality=70&f=650x0',
+        "description": 'for those seeking a fine, soft and very comfortable Beanie at a very low price.',
+        "price": 99.99
+    },
+    {
+        "id": 'p3',
+        "ownerId": 'u2',
+        "title": 'Men’s Waterproof Winter Hiking Jacket - SH100 Warm -5°C Waterproof - Blue',
+        "imageUrl": 'https://contents.mediadecathlon.com/p2172599/31e38371875cbaa821ecf41aaf551f63/p2172599.jpg?format=auto&quality=70&f=650x0',
+        "description": 'Want to explore snow-covered trails? We have designed this waterproof and warm jacket down to -5°C while reducing its environmental impact.',
+        "price": 8.99
+    },
+    {
+        "id": 'p4',
+        "ownerId": 'u3',
+        "title": 'Mountain Trekking Recycled Polyester Liner Gloves 100 Black',
+        "imageUrl": 'https://contents.mediadecathlon.com/p1013819/a8f1f310cbdc4987d954e05f1a80238f/p1013819.jpg?format=auto&quality=70&f=650x0',
+        "description": "Designed by trekking enthusiasts, these recycled polyester liner gloves give you the warmth and durability you need for walking in cool weather.",
+        "price": 15.99
+    },
+    {
+        "id": 'p5',
+        "ownerId": 'u3',
+        "title": 'Men\'s Warm Water-Repellent Hiking Trousers - SH100 ULTRA-WARM',
+        "title": 'https://contents.mediadecathlon.com/p1927253/6b29fecf94c04d253bb4769b635fb377/p1927253.jpg?format=auto&quality=70&f=650x0',
+        "description": 'Perfect trousers for hiking in extremely cold weather. Extremely warm fleece inner and water-repellent outer fabric will give you comfort.',
+        "price": 2299.99
+    },
+    {
+        "id": 'p6',
+        "ownerId": 'u1',
+        "title": 'Men\'s Hiking Fleece Sleeveless Jacket MH120',
+        "imageUrl": 'https://contents.mediadecathlon.com/p1714105/06f86fbdf2a521712ed962f30accae05/p1714105.jpg?format=auto&quality=70&f=650x0',
+        "description": "At the foot of Mont Blanc, our team eco-designed this warm, breathable and affordable fleece gilet for your occasional hikes in the mountains.",
+        "price": 5.49
+    }]
 
 export default PRODUCTS;
